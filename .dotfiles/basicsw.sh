d36a3135e057d6c2bb26d067de839af6ba3ba366
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-01-09 20:13:13 +0100

    :hammer:

--- .dotfiles/basicsw.sh
+++ .dotfiles/basicsw.sh
@@ -8,30 +8,15 @@
 echo "\n\tUPDATE THE APT ... "
 sudo apt-get update -y
 
+echo "\n\nINSTALLING GIT - STALYN ALJEANDRO (saavalencia97@gmail.com) ... "
 sudo apt-get install git -y
-# echo "Git Configuration"
-# echo "Enter global user name for git"
-# read GITUSER;
-# git config --global user.name "${GITUSER}"
-# echo "Enter global user email for git"
-# read GITEMAIL;
-# git config --global user.email "${GITEMAIL}"
+git config --global user.name STALYN ALEJANDRO
+git config --global user.email saavalencia97@gmail.com
 
-echo "\n\nINSTALLGIN JAVA ...  "
-sudo apt-get install default-jre -y
-java --version
-
-echo "\n\nINSTALLING MAVEN .. "
-sudo apt-get install maven -y
-mvn -version
-
-echo "\n\nINSTALLING NODE 16.13.0 LTS ... "
-curl -fsSL https://deb.nodesource.com/setup_16.13.0 | sudo -E bash -
+echo "\n\nINSTALLING NODE 18.x LTS ... "
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 sudo apt install npm -y
-
-#echo "\n\nINSTALLING SNAP ... "
-#sudo apt-get install snapd 
 
 echo "\n\nINSTALLING VIM EDITORS ..."
 sudo apt-get install vim -y
@@ -40,30 +25,6 @@ sudo apt-get install neovim -y
 echo "\n\nINSTALLING VIMPLUG ... "
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-#echo "\n\n INSTALLING INTELLIJ EDITOR ... "
-#sudo snap install intellij-idea-community --classic 
-
-#echo "\n\n INSTALLING SNAP ANDROID STUDIO ... "
-#sudo snap install android-studio --classic 
-
-#echo "\n\n INSTALLING SNAP GNU IMAGE EDITOR ... "
-#sudo snap install gimp 
-
-echo "\n\n INSTALLING SNAP BLENDER .. "
-#sudo snap install blender --classic 
-
-echo "\n\n INSTALLING DOCKER ENGINE ... "
-sudo apt-get install ca-certificates curl gnupg lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-sudo apt-get update -y
-sudo apt-get install docker.io -y 
-
-echo "\n\n INSTALLING INSOMINA REST API ... "
-#sudo snap install insomnia 
-
-echo "\n\n INSTALLING SPOTIFY ... "
-sudo snap install spotify
 
 echo "\n\nINSTALLING POP SHELL ... "
 sudo apt install node-typescript make -y
