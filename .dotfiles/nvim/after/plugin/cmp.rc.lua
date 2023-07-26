039a1185e134c895673bbd096b4f022ef1a587b9
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-07-26 16:41:16 +0200

    Add CMP rempas :fire: :fire: :fire:

--- .dotfiles/nvim/after/plugin/cmp.rc.lua
+++ .dotfiles/nvim/after/plugin/cmp.rc.lua
@@ -1,32 +1,23 @@
 local cmp = require 'cmp'
 
 cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        end,
+    snippet = {},
+    mapping = {
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        }),
+        -- ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        -- ["<C-Space>"] = cmp.mapping.complete(),
+        -- ["<C-e>"] = cmp.mapping.close(),
+        -- ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+        -- ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
     },
-    window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        { name = 'vsnip' }, -- For vsnip users.
-        -- { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
     }, {
         { name = 'buffer' },
     })
@@ -62,7 +53,6 @@ cmp.setup.cmdline(':', {
 
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['tsserver'].setup {
     capabilities = capabilities
 }
--- .dotfiles/nvim/lua/sa/packer.lua
+++ .dotfiles/nvim/lua/sa/packer.lua
@@ -3,9 +3,6 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
-    -- Harpoon
-    use('theprimeagen/harpoon')
-
     -- Terminal
     use {
         "NvChad/nvterm",
@@ -25,7 +22,6 @@ return require('packer').startup(function(use)
     -- Telescope || Search
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.2',
-        -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
@@ -37,33 +33,33 @@ return require('packer').startup(function(use)
         },
     }
 
+    -- Harpoon
+    use('theprimeagen/harpoon')
+
     -- Treesitter || Code Sintax
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
     -- Lualine || Colors
-    use {
-        'nvim-lualine/lualine.nvim',
-        -- requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
+    use 'nvim-lualine/lualine.nvim'
 
     -- LSPSaga || Preview
-    use('glepnir/lspsaga.nvim')
+    use "glepnir/lspsaga.nvim"
 
     -- -- LSP Config
-    use 'neovim/nvim-lspconfig'              -- LSP
-    use 'jose-elias-alvarez/null-ls.nvim'    -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-    use 'williamboman/mason.nvim'
-    use 'williamboman/mason-lspconfig.nvim'
-    use 'hrsh7th/nvim-cmp'                   -- Completion
-    use 'hrsh7th/cmp-buffer'                 -- nvim-cmp source for buffer words
-    use 'hrsh7th/cmp-nvim-lsp'               -- nvim-cmp source for neovim's built-in LSP
+    use "neovim/nvim-lspconfig"           -- LSP
+    use "jose-elias-alvarez/null-ls.nvim" -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+    use "williamboman/mason.nvim"
+    use "williamboman/mason-lspconfig.nvim"
+    use "hrsh7th/nvim-cmp"     -- Completion
+    use "hrsh7th/cmp-buffer"   -- nvim-cmp source for buffer words
+    use "hrsh7th/cmp-nvim-lsp" -- nvim-cmp source for neovim's built-in LSP
 
     -- Theming
-    use("tanvirtin/monokai.nvim")
-    use("patstockwell/vim-monokai-tasty")
-    use("folke/tokyonight.nvim")
-    use("jacoborus/tender.vim")
-    use('maxmx03/dracula.nvim')
+    use "tanvirtin/monokai.nvim"
+    use "patstockwell/vim-monokai-tasty"
+    use "folke/tokyonight.nvim"
+    use "jacoborus/tender.vim"
+    use 'maxmx03/dracula.nvim'
     use {
         'uloco/bluloco.nvim',
         requires = { 'rktjmp/lush.nvim' }
