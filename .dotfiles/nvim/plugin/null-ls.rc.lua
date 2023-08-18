12e203abf0bc214d02677d4a8346123005c6bf89
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-08-18 13:08:13 +0200

    still not working with nvim + eslint + prettier

--- .dotfiles/nvim/plugin/null-ls.rc.lua
+++ .dotfiles/nvim/plugin/null-ls.rc.lua
@@ -3,37 +3,15 @@ if not status then
     return
 end
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-    })
-end
-
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.completion.spell,
-        on_attach = function(client, bufnr)
-            if client.supports_method("textDocument/formatting") then
-                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = augroup,
-                    buffer = bufnr,
-                    callback = function()
-                        lsp_formatting(bufnr)
-                    end,
-                })
-            end
-        end,
     },
-})
-
---
+})--
 -- null_ls.setup {
 --     sources = {
 --         null_ls.builtins.formatting.prettierd,
