e4eaac8343717ad2be5a6f736c09de5ceb7764ce
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-10-26 22:56:49 +0200

    :hammer:

--- .dotfiles/nvim/lua/sa/config/null.lua
+++ .dotfiles/nvim/lua/sa/config/null.lua
@@ -12,13 +12,13 @@ null_ls.setup({
                 group = augroup,
                 buffer = bufnr,
             })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                end,
-            })
+            -- vim.api.nvim_create_autocmd("BufWritePre", {
+            --     group = augroup,
+            --     buffer = bufnr,
+            --     callback = function()
+            --         vim.lsp.buf.format({ bufnr = bufnr })
+            --     end,
+            -- })
         end
     end,
 })
