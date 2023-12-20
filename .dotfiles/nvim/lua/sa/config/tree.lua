7e202d67c25173de31dd1ab3a9ede9d378ce85fb
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-12-20 22:59:55 +0700

    wip tree.lua

--- .dotfiles/nvim/lua/sa/config/tree.lua
+++ .dotfiles/nvim/lua/sa/config/tree.lua
@@ -1,3 +1,7 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+
 local function my_on_attach(bufnr)
     local api = require "nvim-tree.api"
 
@@ -12,101 +16,7 @@ local function my_on_attach(bufnr)
     vim.keymap.set('n', '<C-S-_>', api.tree.collapse_all, opts('Up'))
     vim.keymap.set('n', '<leader>q', api.tree.close, opts('Close'))
 end
---
---
--- local options = {
---     on_attach = my_on_attach,
---     filters = {
---         dotfiles = false,
---         exclude = { vim.fn.stdpath "config" .. "/lua/custom" },
---     },
---     disable_netrw = true,
---     hijack_netrw = true,
---     hijack_cursor = true,
---     hijack_unnamed_buffer_when_opening = false,
---     sync_root_with_cwd = true,
---     update_focused_file = {
---         enable = true,
---         update_root = false,
---     },
---     view = {
---         adaptive_size = false,
---         side = "left",
---         width = 30,
---         preserve_window_proportions = true,
---     },
---     git = {
---         enable = false,
---         ignore = true,
---     },
---     filesystem_watchers = {
---         enable = true,
---     },
---     actions = {
---         open_file = {
---             resize_window = true,
---         },
---     },
---     -- renderer = {
---     --     root_folder_label = false,
---     --     highlight_git = false,
---     --     highlight_opened_files = "none",
---     --
---     --     indent_markers = {
---     --         enable = false,
---     --     },
---     --
---     --     icons = {
---     --         show = {
---     --             file = true,
---     --             folder = true,
---     --             folder_arrow = true,
---     --             git = false,
---     --         },
---     --         glyphs = {
---     --             default = "📄",
---     --             symlink = "🔗",
---     --             folder = {
---     --                 default = "🗂️",
---     --                 empty = "📁",
---     --                 empty_open = "📂",
---     --                 open = "📂",
---     --                 symlink = "🔗",
---     --                 symlink_open = "🔗📂",
---     --                 arrow_open = "↪️ ",
---     --                 arrow_closed = "➡️ ",
---     --             },
---     --             git = {
---     --                 unstaged = "🟡",
---     --                 staged = "🟢",
---     --                 unmerged = "🚩",
---     --                 renamed = "🇷",
---     --                 untracked = "🔶",
---     --                 deleted = "❌",
---     --                 ignored = "🔸",
---     --             },
---     --         },
---     --     },
---     -- },
--- }
---
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
--- vim.opt.termguicolors = true
--- require("nvim-tree").setup(options)
 
-
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
--- empty setup using defaults
--- require("nvim-tree").setup()
-
--- OR setup with some options
 require("nvim-tree").setup({
     on_attach = my_on_attach,
     sort = {
@@ -129,20 +39,7 @@ require("nvim-tree").setup({
                     empty = "📁",
                     empty_open = "📂",
                     open = "📂",
-                    -- symlink = "🔗",
-                    -- symlink_open = "🔗📂",
-                    -- arrow_open = "↪️ ",
-                    -- arrow_closed = "➡️ ",
                 },
-                -- git = {
-                --     unstaged = "🟡",
-                --     staged = "🟢",
-                --     unmerged = "🚩",
-                --     renamed = "🇷",
-                --     untracked = "🔶",
-                --     deleted = "❌",
-                --     ignored = "🔸",
-                -- },
             },
         },
 
