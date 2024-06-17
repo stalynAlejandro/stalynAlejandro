2b6e14519541b5f1ae250de7a2171d56f0e1d8f4
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2024-06-17 21:55:08 +0700

    cc

--- bs.sh
+++ bs.sh
@@ -1,7 +1,11 @@
-
+sudo apt-get install neofetch -y
+sudo apt-get install htop -y
+sudo apt-get install btop -y
 sudo apt-get install git -y
 sudo apt-get install vim -y
 sudo apt-get install neovim -y
+sudo apt-get install dconf-editor -y
+sudo apt-get install icdiff -y
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -15,8 +19,17 @@ chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 ln -s stalynAlejandro/.dotfiles/.zshrc .
 ln -s stalynAlejandro/.dotfiles/.vimrc .
 ln -s stalynAlejandro/.dotfiles/.gitconfig .
 
 curl -fsSL https://ollama.com/install.sh | sh
+
+
+gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
+gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
+
+sudo apt autoremove
